a0297c029aba9754aec3a1a8f755ba95c2750728
mkatay <kmagdi@kkando.hu>
2025-02-16 19:36:12 +0100

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: { chunkSizeWarningLimit: 1600, },
   plugins: [react(),tailwindcss()],
+  
 })
